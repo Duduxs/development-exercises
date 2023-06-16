232adb034ff13f4e2549471d4a594663a6a04da8
Eduardo José <duduxss3@gmail.com>
2023-06-16 10:27:29 -0300

    [main] :sparkles: new task done

---
+++ leetcode/src/leetcode/java/easy/RotateString.java
@@ -0,0 +1,31 @@
+package leetcode.java.easy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RotateString {
+
+    public static void main(String[] args) {
+        System.out.println(rotateString("abcde", "cdeab"));
+    }
+
+    public static boolean rotateString(String s, String goal) {
+
+        if (s.equals(goal)) return true;
+
+        List<Character> string = s.chars().mapToObj(x -> (char) x).collect(Collectors.toList());
+
+        for (var i = 0; i < s.length(); i++) {
+            string.remove(0);
+            string.add(s.length() - 1, s.charAt(i));
+            StringBuilder builder = new StringBuilder(s.length());
+
+            for(Character c : string) {builder.append(c);}
+
+            if (builder.toString().equals(goal)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
