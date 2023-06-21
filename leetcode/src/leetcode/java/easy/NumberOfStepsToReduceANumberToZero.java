1d7e37ba76ccbaa193035fc62dad272df755860c
Eduardo José <duduxss3@gmail.com>
2023-06-21 11:19:33 -0300

    [main] :art: Some code refactor on new task

--- leetcode/src/leetcode/java/easy/NumberOfStepsToReduceANumberToZero.java
+++ leetcode/src/leetcode/java/easy/NumberOfStepsToReduceANumberToZero.java
@@ -5,22 +5,10 @@ public class NumberOfStepsToReduceANumberToZero {
     public static int numberOfSteps(int num) {
 
         var numbersOfSteps = 0;
-        var currentNumber = num;
 
-        while (true) {
-
-            if (currentNumber == 0) break;
-
-            try {
-                if (currentNumber % 2 == 0) {
-                    currentNumber /= 2;
-                } else {
-                    currentNumber -= 1;
-                }
-            } finally {
-                numbersOfSteps++;
-            }
-
+        while (num != 0) {
+            num = num % 2 == 0 ? num / 2 : num - 1;
+            numbersOfSteps++;
         }
 
         return numbersOfSteps;
