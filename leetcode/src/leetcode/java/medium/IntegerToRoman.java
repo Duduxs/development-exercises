77649347fa1dbec0d67013ecdeb0aaa3b72652b6
Eduardo José <duduxss3@gmail.com>
2023-06-27 06:49:18 -0300

    Modifying the algorithm of IntegerToRoman

--- leetcode/src/leetcode/java/medium/IntegerToRoman.java
+++ leetcode/src/leetcode/java/medium/IntegerToRoman.java
@@ -1,40 +1,43 @@
 package leetcode.java.medium;
 
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Map;
 
 public class IntegerToRoman {
 
     public static String intToRoman(int num) {
-        Map<String, Integer> data = new HashMap<>(
-                Map.of(
-                        "M", 1000,
-                        "CM", 900,
-                        "D", 500,
-                        "CD", 400,
-                        "C", 100,
-                        "XC", 90,
-                        "L", 50,
-                        "XL", 40,
-                        "X", 10,
-                        "IX", 9
-                ));
-
-        data.putAll(Map.of("V", 5, "IV", 4, "III", 3, "II", 2, "I", 1));
+        Map<Integer, String> data = Map.ofEntries(
+                Map.entry(1000, "M"),
+                Map.entry(900, "CM"),
+                Map.entry(500, "D"),
+                Map.entry(400, "CD"),
+                Map.entry(100, "C"),
+                Map.entry(90, "XC"),
+                Map.entry(50, "L"),
+                Map.entry(40, "XL"),
+                Map.entry(10, "X"),
+                Map.entry(9, "IX"),
+                Map.entry(5, "V"),
+                Map.entry(4, "IV"),
+                Map.entry(1, "I")
+        );
 
         var convertedRoman = new StringBuilder();
 
-        var sortedEntrySet = data.entrySet().stream().sorted(Comparator.comparingInt(x -> -x.getValue())).toList();
+        var sortedEntrySet = data.entrySet()
+                .stream()
+                .sorted(Comparator.comparingInt(x -> -x.getKey()))
+                .toList();
 
         for (var entry : sortedEntrySet) {
 
-            while (num - entry.getValue() > 0) {
-                num -= entry.getValue();
-                convertedRoman.append(entry.getKey());
+            while (num - entry.getKey() > 0) {
+                num -= entry.getKey();
+                convertedRoman.append(entry.getValue());
             }
-            if (num - entry.getValue() == 0) {
-                convertedRoman.append(entry.getKey());
+
+            if (num - entry.getKey() == 0) {
+                convertedRoman.append(entry.getValue());
                 break;
             }
         }
