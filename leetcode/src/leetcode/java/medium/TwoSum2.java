15ce936f9b481ec16858d31c333b07b554257f71
Eduardo José <duduxss3@gmail.com>
2023-07-13 09:43:33 -0300

    Appliyng two pointers method in task twosum2

--- leetcode/src/leetcode/java/medium/TwoSum2.java
+++ leetcode/src/leetcode/java/medium/TwoSum2.java
@@ -1,7 +1,10 @@
 package leetcode.java.medium;
 
+import java.util.Arrays;
+
 public class TwoSum2 {
 
+    //Brute force O(N²)
     public static int[] twoSum(int[] numbers, int target) {
 
         var result = new int[2];
@@ -22,7 +25,28 @@ public class TwoSum2 {
         return result;
     }
 
+    //Two pointers
+    //O(N)
+    public static int[] twoSumPointers(int[] numbers, int target) {
+
+        var i = 0;
+        var j = numbers.length - 1;
+
+        while (i < j) {
+
+            var sum = numbers[i] + numbers[j];
+
+            if (sum > target) j--;
+            else if (sum < target) i++;
+            else return new int[]{i + 1, j + 1};
+
+        }
+
+        return new int[]{0, 0};
+    }
+
     public static void main(String[] args) {
         twoSum(new int[]{1, 2, 3, 4, 5, 6}, 3);
+        System.out.println(Arrays.toString(twoSumPointers(new int[]{1, 2, 3, 4, 5, 6}, 3)));
     }
 }
