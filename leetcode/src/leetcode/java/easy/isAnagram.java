d6a16101e24129b59c54a9fc29dd055c7be945cb
Eduardo José <duduxss3@gmail.com>
2023-07-18 10:10:07 -0300

    IsAnagram task done

---
+++ leetcode/src/leetcode/java/easy/isAnagram.java
@@ -0,0 +1,31 @@
+package leetcode.java.easy;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class isAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+
+        if(t.length() != s.length()) return false;
+
+        Queue<Character> pattern = new ArrayDeque<>();
+        for (var c : s.toCharArray()) {
+            pattern.add(c);
+        }
+
+        for (var i = 0; i < t.length(); i++) {
+            if (pattern.contains(t.charAt(i))) {
+                pattern.remove(t.charAt(i));
+            } else return false;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("aacc", "ccac"));
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println(isAnagram("rat", "car"));
+    }
+}
