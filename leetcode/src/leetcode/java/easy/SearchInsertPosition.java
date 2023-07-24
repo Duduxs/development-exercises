ba392ff10c72c5c8cddf37ce9c5bb3235a0e6ad1
Eduardo José <duduxss3@gmail.com>
2023-07-23 21:27:51 -0300

    Adding new task with binary search

---
+++ leetcode/src/leetcode/java/easy/SearchInsertPosition.java
@@ -0,0 +1,31 @@
+package leetcode.java.easy;
+
+public class SearchInsertPosition {
+
+    public static int searchInsert(int[] nums, int target) {
+
+        var firstPosition = 0;
+        var lastPosition = nums.length - 1;
+
+        while (firstPosition <= lastPosition) {
+
+            var mid = firstPosition + (lastPosition - firstPosition) / 2;
+
+            if (target > nums[mid]) firstPosition = mid + 1;
+            else if (target < nums[mid]) lastPosition = mid - 1;
+            else return mid;
+       }
+
+        return firstPosition;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 2));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 5));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 0));
+
+
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 6));
+    }
+}
