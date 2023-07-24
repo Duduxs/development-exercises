a4e795b755f5ebd94adcd2ff8d169f45364997aa
Eduardo José <duduxss3@gmail.com>
2023-07-23 21:31:33 -0300

    Adding helper comment

--- leetcode/src/leetcode/java/easy/SearchInsertPosition.java
+++ leetcode/src/leetcode/java/easy/SearchInsertPosition.java
@@ -2,6 +2,7 @@ package leetcode.java.easy;
 
 public class SearchInsertPosition {
 
+    //Binary Search BIG O(LOG N)
     public static int searchInsert(int[] nums, int target) {
 
         var firstPosition = 0;
@@ -14,13 +15,14 @@ public class SearchInsertPosition {
             if (target > nums[mid]) firstPosition = mid + 1;
             else if (target < nums[mid]) lastPosition = mid - 1;
             else return mid;
-       }
+        }
 
         return firstPosition;
     }
 
     public static void main(String[] args) {
 
+        System.out.println(searchInsert(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 7));
         System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 2));
         System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 5));
         System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 0));
