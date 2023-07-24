50c5a481a7e7fba97352864de75c9b635a2af7f8
Eduardo J <duduxss3@gmail.com>
2023-07-24 16:56:06 -0300

    More concise task of isHappy

--- leetcode/src/leetcode/java/easy/IsHappy.java
+++ leetcode/src/leetcode/java/easy/IsHappy.java
@@ -29,10 +29,45 @@ public class IsHappy {
 
     }
 
+    public static boolean isHappyTwo(int n) {
+        if (n < 0) return false;
+
+        Set<Integer> computedNumbers = new HashSet<>();
+        var sum = 0;
+
+        while (true) {
+
+            sum = 0;
+
+            while (n != 0) {
+                var digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+
+            if (n == 1) break;
+            if (computedNumbers.contains(n)) return false;
+
+            computedNumbers.add(n);
+
+        }
+
+        return true;
+
+    }
+
     public static void main(String[] args) {
+        System.out.println(isHappyTwo(19));
+        System.out.println(isHappyTwo(78));
+        System.out.println(isHappyTwo(7));
+
         System.out.println(isHappy(19));
         System.out.println(isHappy(78));
         System.out.println(isHappy(7));
+
+
 
     }
 
