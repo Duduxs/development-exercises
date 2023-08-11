c28aec12ac23d0c9202e0e0af59de3fde2411b7f
Eduardo J <duduxss3@gmail.com>
2023-08-11 09:17:24 -0300

    Second solution done

--- leetcode/src/leetcode/sql/easy/rising-temperature.sql
+++ leetcode/src/leetcode/sql/easy/rising-temperature.sql
@@ -5,4 +5,10 @@ insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25
 insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20');
 insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30');
 
+-- First Solution
 SELECT w2.id from Weather w1, Weather w2 where w2.temperature > w1.temperature and w1.recordDate = date_sub(w2.recordDate, INTERVAL 1 day);
+
+-- Second Solution
+SELECT w1.id from Weather w1
+inner join Weather w2 on w1.recordDate = DATE_ADD(w2.recordDate, interval 1 day)
+where w1.temperature > w2.temperature ;
