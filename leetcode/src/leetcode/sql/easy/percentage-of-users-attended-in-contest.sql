97832b4b805be31115d57b9e094eaa9e7f21ccdd
Eduardo José <duduxss3@gmail.com>
2023-08-22 10:27:32 +0000

    New task done

---
+++ leetcode/src/leetcode/sql/easy/percentage-of-users-attended-in-contest.sql
@@ -0,0 +1,26 @@
+Create table If Not Exists Users (user_id int, user_name varchar(20))
+Create table If Not Exists Register (contest_id int, user_id int)
+Truncate table Users
+insert into Users (user_id, user_name) values ('6', 'Alice')
+insert into Users (user_id, user_name) values ('2', 'Bob')
+insert into Users (user_id, user_name) values ('7', 'Alex')
+Truncate table Register
+insert into Register (contest_id, user_id) values ('215', '6')
+insert into Register (contest_id, user_id) values ('209', '2')
+insert into Register (contest_id, user_id) values ('208', '2')
+insert into Register (contest_id, user_id) values ('210', '6')
+insert into Register (contest_id, user_id) values ('208', '6')
+insert into Register (contest_id, user_id) values ('209', '7')
+insert into Register (contest_id, user_id) values ('209', '6')
+insert into Register (contest_id, user_id) values ('215', '7')
+insert into Register (contest_id, user_id) values ('208', '7')
+insert into Register (contest_id, user_id) values ('210', '2')
+insert into Register (contest_id, user_id) values ('207', '2')
+insert into Register (contest_id, user_id) values ('210', '7')
+
+-- First solution
+
+select r.contest_id, round(((count(*) / total) * 100), 2) as percentage  from Register r
+cross join (select count(*) as total from Users u) as total
+group by r.contest_id, total
+order by percentage desc, r.contest_id asc;
