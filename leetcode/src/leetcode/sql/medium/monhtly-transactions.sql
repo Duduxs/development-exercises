03ee42bbfada15a135b83f42158cc08056c6a814
Eduardo José <duduxss3@gmail.com>
2023-08-24 11:55:59 +0000

    second solution for the new task

--- leetcode/src/leetcode/sql/medium/monhtly-transactions.sql
+++ leetcode/src/leetcode/sql/medium/monhtly-transactions.sql
@@ -16,3 +16,15 @@ sum(amount) as trans_total_amount,
 sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
 from Transactions
 group by month, country;
+
+-- Second solution short syntax sum
+
+select 
+DATE_FORMAT(trans_date, '%Y-%m') as month, 
+country, 
+count(*) as trans_count, 
+sum(state = 'approved') as approved_count, 
+sum(amount) as trans_total_amount,
+sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by month, country;
