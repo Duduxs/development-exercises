d16eeff9b2a0609f444625b6f9c8db9d682ff375
Eduardo José <duduxss3@gmail.com>
2023-09-13 11:38:25 +0000

    Adding other solution

--- leetcode/src/leetcode/sql/easy/the-number-of-employees-wich-report-to-each-employee.sql
+++ leetcode/src/leetcode/sql/easy/the-number-of-employees-wich-report-to-each-employee.sql
@@ -12,3 +12,11 @@ inner join Employees e2 on e1.employee_id = e2.reports_to
 group by e1.employee_id
 having count(*) > 0
 order by e1.employee_id
+
+-- Second solution
+
+select e1.employee_id, e1.name, count(*) as reports_count, round(sum(e2.age) /count(*)) as average_age from Employees e1, Employees e2
+where e1.employee_id = e2.reports_to
+group by 1
+having reports_count > 0
+order by 1
