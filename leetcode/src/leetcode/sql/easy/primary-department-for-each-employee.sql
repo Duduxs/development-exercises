59163d761f146419055045d12d8302e8a307704d
Eduardo José <duduxss3@gmail.com>
2023-09-18 11:55:24 +0000

    new task done

---
+++ leetcode/src/leetcode/sql/easy/primary-department-for-each-employee.sql
@@ -0,0 +1,15 @@
+Create table If Not Exists Employee (employee_id int, department_id int, primary_flag ENUM('Y','N'))
+Truncate table Employee
+insert into Employee (employee_id, department_id, primary_flag) values ('1', '1', 'N')
+insert into Employee (employee_id, department_id, primary_flag) values ('2', '1', 'Y')
+insert into Employee (employee_id, department_id, primary_flag) values ('2', '2', 'N')
+insert into Employee (employee_id, department_id, primary_flag) values ('3', '3', 'N')
+insert into Employee (employee_id, department_id, primary_flag) values ('4', '2', 'N')
+insert into Employee (employee_id, department_id, primary_flag) values ('4', '3', 'Y')
+insert into Employee (employee_id, department_id, primary_flag) values ('4', '4', 'N')
+
+-- First Solution
+
+select e1.employee_id, e1.department_id from Employee e1
+where e1.primary_flag = 'Y' or (select count(department_id) = 1 from Employee where employee_id = e1.employee_id)
+group by e1.employee_id
