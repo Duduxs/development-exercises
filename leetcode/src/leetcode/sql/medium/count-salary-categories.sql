5223772ef71df0d966e0c181d086d62625b9c76c
Eduardo José <duduxss3@gmail.com>
2023-10-06 15:01:24 +0000

    second solution added for the new task

--- leetcode/src/leetcode/sql/medium/count-salary-categories.sql
+++ leetcode/src/leetcode/sql/medium/count-salary-categories.sql
@@ -11,3 +11,21 @@ union all
 select 'Average Salary' as category, count(*) as accounts_count from Accounts where income between 20000 and 50000
 union all
 select 'High Salary' as category, count(*) as accounts_count from Accounts where income > 50000
+
+-- Second Solution (+ performance and readability)
+
+select
+    c.category,
+    count(a.account_id) as accounts_count
+from (
+    select 'Low Salary' as category union all
+    select 'Average Salary' as category union all
+    select 'High Salary' as category
+) c
+left outer join Accounts a 
+    on (
+        (c.category = 'Low Salary' and a.income < 20000) ||
+        (c.category = 'Average Salary' and a.income between 20000 and 50000) ||
+        (c.category = 'High Salary' and a.income > 50000)
+    )
+group by c.category
