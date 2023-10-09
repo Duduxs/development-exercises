6e8a9bef9cd9d8a838f6bed72223586747a17253
Eduardo José <duduxss3@gmail.com>
2023-10-09 11:48:13 +0000

    new solution for the new task done

--- leetcode/src/leetcode/sql/easy/employees-whose-manager-left-the-company.sql
+++ leetcode/src/leetcode/sql/easy/employees-whose-manager-left-the-company.sql
@@ -14,3 +14,11 @@ where e1.salary < 30000 and e1.manager_id is not null and (
     e1.manager_id not in (select e2.employee_id from Employees e2 where e2.employee_id = e1.manager_id)
 )
 order by 1 asc;
+
+-- Second Solution (lower performance due to not having where, in other words binary search by index)
+
+select e1.employee_id from Employees e1
+where e1.salary < 30000 and e1.manager_id is not null and (
+e1.manager_id not in (select e2.employee_id from Employees e2)
+)
+order by e1.employee_id;
