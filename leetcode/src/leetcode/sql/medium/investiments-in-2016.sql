7b6c14eeb85ce1360116727252f207f11f1c3972
Eduardo José <duduxss3@gmail.com>
2023-10-25 14:25:10 +0000

    adding a new solution

--- leetcode/src/leetcode/sql/medium/investiments-in-2016.sql
+++ leetcode/src/leetcode/sql/medium/investiments-in-2016.sql
@@ -16,3 +16,17 @@ and (i1.lat, i1.lon) not in (
     select i2.lat, i2.lon from Insurance i2
     where i2.pid != i1.pid
 )
+
+-- Second solution with cte
+
+with insurance_filtered_data_cte as (
+select round(sum(i1.tiv_2016),2) as tiv_2016 from Insurance i1
+where i1.tiv_2015 in (
+    select i2.tiv_2015 from Insurance i2
+    where i2.pid != i1.pid
+)
+and (i1.lat, i1.lon) not in (
+    select i2.lat, i2.lon from Insurance i2
+    where i2.pid != i1.pid
+))
+select tiv_2016 from insurance_filtered_data_cte
