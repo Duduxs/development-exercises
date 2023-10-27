ff4751df967d0e9ee3a94b14bf50f143946e9adf
Eduardo José <duduxss3@gmail.com>
2023-10-27 14:28:18 +0000

    new task done

---
+++ leetcode/src/leetcode/sql/medium/consecutive-numbers.sql
@@ -0,0 +1,18 @@
+Create table If Not Exists Logs (id int, num int)
+Truncate table Logs
+insert into Logs (id, num) values ('1', '1')
+insert into Logs (id, num) values ('2', '1')
+insert into Logs (id, num) values ('3', '1')
+insert into Logs (id, num) values ('4', '2')
+insert into Logs (id, num) values ('5', '1')
+insert into Logs (id, num) values ('6', '2')
+insert into Logs (id, num) values ('7', '2')
+
+-- First Solution
+
+select distinct cn1.num as ConsecutiveNums from Logs cn1
+where cn1.num = (
+    select cn2.num from Logs cn2 where cn2.id = cn1.id + 1
+) and cn1.num = (
+    select cn2.num from Logs cn2 where cn2.id = cn1.id + 2
+)
