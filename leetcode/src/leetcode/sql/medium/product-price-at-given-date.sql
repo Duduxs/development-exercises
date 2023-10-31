68a8872a7d022e644d8b290d18bedcd22cbb685d
Eduardo José <duduxss3@gmail.com>
2023-10-30 22:46:59 -0300

    new solution done to the new task

--- leetcode/src/leetcode/sql/medium/product-price-at-given-date.sql
+++ leetcode/src/leetcode/sql/medium/product-price-at-given-date.sql
@@ -23,3 +23,40 @@ from Products p2 inner join
 	group by p3.product_id 
 ) as r
 on p2.product_id = r.product_id and p2.change_date  = r.max_date;
+
+-- Second Solution
+
+with 
+	products_with_closest_date as (
+		select product_id, max(change_date) as change_date from Products
+		where change_date <= '2019-08-16'
+		group by product_id
+	),
+	products_with_overtaken_date as (
+		select product_id, 10 as price from Products
+		group by product_id
+		having min(change_date) > '2019-08-16'
+	)
+select * from products_with_overtaken_date
+union 
+select product_id, new_price as price from Products
+inner join products_with_closest_date x using (product_id, change_date);
+
+-- Third solution (without using distinct therefore only union all not union)
+
+with 
+	products_with_closest_date as (
+		select product_id, max(change_date) as change_date from Products
+		where change_date <= '2019-08-16'
+		group by product_id
+	),
+	products_with_overtaken_date as (
+		select product_id, 10 as price from Products
+		group by product_id
+		having min(change_date) > '2019-08-16'
+	)
+select * from products_with_overtaken_date
+union all
+select product_id, new_price as price from Products
+inner join products_with_closest_date x using (product_id, change_date);
+
