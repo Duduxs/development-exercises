6b3f7f052a408e7955c841d4c4f62c011a14db49
Eduardo José <duduxss3@gmail.com>
2023-11-08 21:10:45 +0000

    new solution added

--- leetcode/src/leetcode/sql/easy/list-the-products-ordered-in-a-period.sql
+++ leetcode/src/leetcode/sql/easy/list-the-products-ordered-in-a-period.sql
@@ -27,3 +27,12 @@ inner join Orders using (product_id)
 where order_date between '2020-02-01' and '2020-02-29'
 group by product_id
 having sum(unit) >= 100
+
+-- Second Solution
+
+select p.product_name, sum(o.unit) as unit from Products p
+inner join Orders o 
+  on p.product_id = o.product_id 
+  and o.order_date >= '2020-02-01' and o.order_date <= '2020-02-29'
+group by p.product_id
+having sum(unit) >= 100
