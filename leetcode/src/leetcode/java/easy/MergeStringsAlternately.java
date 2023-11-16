4f16ba709297ef94ce50ead8bf821e3e6f798579
Eduardo José <duduxss3@gmail.com>
2023-11-16 10:36:50 -0300

    adding a new solution

--- leetcode/src/leetcode/java/easy/MergeStringsAlternately.java
+++ leetcode/src/leetcode/java/easy/MergeStringsAlternately.java
@@ -2,5 +2,23 @@ package leetcode.java.easy;
 
 public class Test {
 
+    public String mergeAlternately(String word1, String word2) {
+        var longestWord = word1.length() > word2.length() ? word1 : word2;
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < longestWord.length(); i++) {
+            if (word1.length() > i) sb.append(word1.charAt(i));
+            if (word2.length() > i) sb.append(word2.charAt(i));
+        }
+
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+        var test = new Test();
+        System.out.println(test.mergeAlternately("ab", "pqrs"));
+
+    }
     
 }
