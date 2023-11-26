81223bdad075a56b1767e356627c69b17b414ed2
Eduardo José <duduxss3@gmail.com>
2023-11-26 21:25:37 +0000

    adding a second solution

--- leetcode/src/leetcode/sql/easy/combine-two-tables.sql
+++ leetcode/src/leetcode/sql/easy/combine-two-tables.sql
@@ -13,3 +13,8 @@ select p.firstName, p.lastName, a.city, a.state
 from Person p
 left outer join Address a using (personId)
 
+-- Second Solution
+
+select p.firstName, p.lastName, a.city, a.state
+from Address a 
+right outer join Person p on a.personId = p.personId
