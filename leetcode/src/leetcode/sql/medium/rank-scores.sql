e966ed3dbdf18c08685a7d02b4a24ffb41534c16
Eduardo José <duduxss3@gmail.com>
2023-11-28 13:15:07 +0000

    new task done

---
+++ leetcode/src/leetcode/sql/medium/rank-scores.sql
@@ -0,0 +1,12 @@
+Create table If Not Exists Scores (id int, score DECIMAL(3,2))
+Truncate table Scores
+insert into Scores (id, score) values ('1', '3.5')
+insert into Scores (id, score) values ('2', '3.65')
+insert into Scores (id, score) values ('3', '4.0')
+insert into Scores (id, score) values ('4', '3.85')
+insert into Scores (id, score) values ('5', '4.0')
+insert into Scores (id, score) values ('6', '3.65')
+
+-- First Solution
+
+select score, dense_rank() over(order by score desc) as rank from Scores;
