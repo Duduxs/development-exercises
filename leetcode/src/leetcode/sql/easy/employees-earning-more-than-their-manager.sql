87a60c65f489ef7caec763c59ff4f006c53cd460
Eduardo José <duduxss3@gmail.com>
2023-11-29 12:54:29 +0000

    Adding a first solution

---
+++ leetcode/src/leetcode/sql/easy/employees-earning-more-than-their-manager.sql
@@ -0,0 +1,11 @@
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int)
+Truncate table Employee
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3')
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4')
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', 'None')
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', 'None')
+
+-- First Solution
+
+select e.name as Employee from Employee e 
+where e.salary > (select e2.salary from Employee e2 where e2.id = e.managerId)
