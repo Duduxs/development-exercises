07094b5dec6a7eb7baeac6942dad97829d4115aa
Eduardo José <duduxss3@gmail.com>
2023-11-30 15:52:27 -0300

    Adding other soltuion

--- leetcode/src/leetcode/java/easy/KidsWithTheGreatestNumberOfCandies.java
+++ leetcode/src/leetcode/java/easy/KidsWithTheGreatestNumberOfCandies.java
@@ -1,16 +1,12 @@
 package leetcode.java.easy;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class KidsWithTheGreatestNumberOfCandies {
 
     public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-
-        List<Integer> list = Arrays.stream(candies).sorted().collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-        var greatestElement = list.get(list.size() - 1);
-
+        var greatestElement = Arrays.stream(candies).max().getAsInt();
         return Arrays.stream(candies).mapToObj(candy -> candy + extraCandies >= greatestElement).toList();
     }
 
