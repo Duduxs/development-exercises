d96d3376139a5679e7962fe5d33271c578d189f8
Eduardo José <duduxss3@gmail.com>
2023-12-22 19:39:25 +0000

    other solution added

--- leetcode/src/leetcode/sql/easy/sales-person.sql
+++ leetcode/src/leetcode/sql/easy/sales-person.sql
@@ -25,3 +25,11 @@ where s.sales_id not in ( select o.sales_id from Orders o
 left outer join Company c on c.com_id = o.com_id 
 where c.name = 'RED'
 )
+
+-- Second Solution
+
+select s.name from SalesPerson s
+where s.sales_id not in ( select o.sales_id from Orders o
+inner join Company c on c.com_id = o.com_id 
+where c.name = 'RED'
+)
