9ca2710412928e3011f135074ee16e9d58123de2
Eduardo José <duduxss3@gmail.com>
2024-01-03 11:22:52 +0000

    one more slution

--- leetcode/src/leetcode/sql/easy/customer-placing-the-largest-numbers-of-orders.sql
+++ leetcode/src/leetcode/sql/easy/customer-placing-the-largest-numbers-of-orders.sql
@@ -21,3 +21,10 @@ where customer_number = (
     group by customer_number
     order by count(*) desc
     limit 1
+
+-- Third Solution
+
+select customer_number from Orders 
+group by customer_number
+order by count(customer_number) desc
+limit 1
