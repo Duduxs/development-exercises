bad2a43cb19377b353d4e55157b0a8bc3ac039ae
Eduardo J <duduxss3@gmail.com>
2024-01-14 17:26:38 -0300

    other slution added

---
+++ leetcode/src/leetcode/sql/easy/find-total-time-spent-by-each-employee.sql
@@ -0,0 +1,12 @@
+Create table If Not Exists Employees(emp_id int, event_day date, in_time int, out_time int)
+Truncate table Employees
+insert into Employees (emp_id, event_day, in_time, out_time) values ('1', '2020-11-28', '4', '32')
+insert into Employees (emp_id, event_day, in_time, out_time) values ('1', '2020-11-28', '55', '200')
+insert into Employees (emp_id, event_day, in_time, out_time) values ('1', '2020-12-3', '1', '42')
+insert into Employees (emp_id, event_day, in_time, out_time) values ('2', '2020-11-28', '3', '33')
+insert into Employees (emp_id, event_day, in_time, out_time) values ('2', '2020-12-9', '47', '74')
+
+-- First Solution
+
+select e.event_day as day, e.emp_id, sum(e.out_time - e.in_time) as total_time from Employees e
+group by e.event_day, e.emp_id
