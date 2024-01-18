dd998c6e07d0ec4a8bf75624b7bfadcb08c90bc0
Eduardo J <duduxss3@gmail.com>
2024-01-18 08:19:42 -0300

    fixing second slution

--- leetcode/src/leetcode/sql/easy/the-latest-login-in-2020.sql
+++ leetcode/src/leetcode/sql/easy/the-latest-login-in-2020.sql
@@ -20,8 +20,9 @@ group by user_id
 -- Second Solution
 
 select user_id, max(time_stamp) as last_stamp from Logins
-where time_stamp between '2020-01-01' and '2020-12-31'
+where time_stamp between '2020-01-01 00:00:00' and '2020-12-31 23:59:59'
 group by user_id
+
 
 
 -- Third Solution
