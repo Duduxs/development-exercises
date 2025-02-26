0b585146ca2fad0c6f792aeabcce8e3ab57c39b2
Eduardo J <duduxss3@gmail.com>
2025-02-26 08:18:13 -0300

    :art: (main) Deixa solução mais coesa

--- leetcode/src/leetcode/java/easy/AddDigits.java
+++ leetcode/src/leetcode/java/easy/AddDigits.java
@@ -3,31 +3,27 @@ package leetcode.java.easy;
 public class AddDigits {
 
     public static void main(String[] args) {
-        System.out.println(addDigits(38));;
+        System.out.println(addDigits(38));
     }
 
     public static int addDigits(int num) {
         var numAsStr = String.valueOf(num);
-
-        if (numAsStr.length() == 1) return num;
-
         int sum = 0;
-        int i = 0 ;
+        int i = 0;
 
-        while (numAsStr.length() > 1) {
-            if(numAsStr.length() > i) {
-                sum += Integer.valueOf(String.valueOf(numAsStr.charAt(i)));
+        while (true) {
+            if (numAsStr.length() > i) {
+                sum += Integer.parseInt(String.valueOf(numAsStr.charAt(i)));
                 i++;
+            } else if (numAsStr.length() == 1) {
+                break;
             } else {
                 numAsStr = String.valueOf(sum);
-                if(numAsStr.length() == 1) return sum;
                 i = 0;
                 sum = 0;
             }
         }
 
         return sum;
-
     }
-
 }
