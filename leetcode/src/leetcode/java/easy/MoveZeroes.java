f21523fa0bb1a4738f8df240151951a52b76fea1
Eduardo J <duduxss3@gmail.com>
2025-03-04 02:30:03 -0300

    :sparkles: (main) Refatora task +1 vez

--- leetcode/src/leetcode/java/easy/MoveZeroes.java
+++ leetcode/src/leetcode/java/easy/MoveZeroes.java
@@ -1,28 +1,27 @@
 package leetcode.java.easy;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MoveZeroes {
     public static void main(String[] args) {
-       moveZeroes(new int[] { 0, 1, 0 ,3 ,12 });
+        moveZeroes(new int[]{0, 1, 0, 3, 12});
     }
 
     public static void moveZeroes(int[] nums) {
         var zeroesQuantities = 0;
         List<Integer> numbers = new ArrayList<>();
+
         for (int num : nums) {
             if (num == 0) zeroesQuantities++;
             else numbers.add(num);
         }
 
-        while(zeroesQuantities != 0) {
-            numbers.add(0);
-            zeroesQuantities--;
-        }
-
-        for(var i = 0 ; i < nums.length ; i++) {
+        for (var i = 0; i < nums.length; i++) {
+            if (zeroesQuantities != 0) {
+                numbers.add(0);
+                zeroesQuantities--;
+            }
             nums[i] = numbers.get(i);
         }
     }
