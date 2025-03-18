559b2f37e50d8d7abc8ed6b6a2601859b680ae89
Eduardo J <duduxss3@gmail.com>
2025-03-18 07:21:01 -0300

    :sparkles: (main) Resolve problema do cpf

--- custom/java/curso-eda-nelio/src/strings/exercises/CPF.java
+++ custom/java/curso-eda-nelio/src/strings/exercises/CPF.java
@@ -3,6 +3,22 @@ package src.strings.exercises;
 public class CPF {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(sanitize("123.456.789-09"));
+        System.out.println(sanitize("87409217293"));
+        System.out.println(sanitize("874092172-93"));
+        System.out.println(sanitize("874.092.172-93"));
+    }
+
+    public static String sanitize(String cpf) {
+        final var sb = new StringBuilder();
+
+        for (var i = 0; i < cpf.length(); i++) {
+            var c = cpf.charAt(i);
+            if (Character.isDigit(c)) {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
     }
 }
