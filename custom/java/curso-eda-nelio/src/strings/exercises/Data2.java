255dc24ca0bfae4ea5ce38ac5513a252530e0024
Eduardo J <duduxss3@gmail.com>
2025-03-18 07:37:23 -0300

    :sparkles: (main) Adiciona 2 solução para o problema Data2

--- custom/java/curso-eda-nelio/src/strings/exercises/Data2.java
+++ custom/java/curso-eda-nelio/src/strings/exercises/Data2.java
@@ -5,11 +5,15 @@ public class Data2 {
 
     public static void main(String[] args) {
         System.out.println(formatDate(21, 7, 2010));
+        System.out.println(formatDate2(21, 7, 2010));
     }
 
     public static String formatDate(int day, int month, int year) {
         var formattedDay = String.valueOf(day).length() == 1 ? "0" + day : day;
         var formattedMonth = String.valueOf(month).length() == 1 ? "0" + month : month;
         return String.format("%s/%s/%s", formattedDay, formattedMonth, year);
+    }
+    public static String formatDate2(int day, int month, int year) {
+        return String.format("%02d/%02d/%d", day, month, year);
     }
 }
