621eba7ff7904b7e0a985beef53227b5a61c6e10
Eduardo J <duduxss3@gmail.com>
2025-03-25 08:24:00 -0300

    :sparkles: (main) Add +1 impl da Data2 e começa a resolver problema de Transactions

--- custom/java/curso-eda-nelio/src/strings/exercises/Data2.java
+++ custom/java/curso-eda-nelio/src/strings/exercises/Data2.java
@@ -6,6 +6,7 @@ public class Data2 {
     public static void main(String[] args) {
         System.out.println(formatDate(21, 7, 2010));
         System.out.println(formatDate2(21, 7, 2010));
+        System.out.println(formatDate3(21, 7, 2010));
     }
 
     public static String formatDate(int day, int month, int year) {
@@ -15,5 +16,15 @@ public class Data2 {
     }
     public static String formatDate2(int day, int month, int year) {
         return String.format("%02d/%02d/%d", day, month, year);
+    }
+    public static String formatDate3(int day, int month, int year) {
+        return String.format("%s/%s/%s", addZero(day), addZero(month), year);
+    }
+
+    public static String addZero(int n) {
+        if (n < 10) {
+            return "0" + n;
+        }
+        return String.valueOf(n);
     }
 }
