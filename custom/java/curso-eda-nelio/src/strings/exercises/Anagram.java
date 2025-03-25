9d9ee14dcd51ad487c2442c425981ad29fef27fd
Eduardo J <duduxss3@gmail.com>
2025-03-25 09:11:23 -0300

    :sparkles: (main) Adiciona 2 solução do isAnagram

--- custom/java/curso-eda-nelio/src/strings/exercises/Anagram.java
+++ custom/java/curso-eda-nelio/src/strings/exercises/Anagram.java
@@ -7,6 +7,11 @@ public class Anagram {
     public static void main(String[] args) {
         System.out.println(isAnagram("anagram", "nagaram"));
         System.out.println(isAnagram("rat", "car"));
+
+        System.out.println();
+
+        System.out.println(isAnagram2("anagram", "nagaram"));
+        System.out.println(isAnagram2("rat", "car"));
     }
 
     public static boolean isAnagram(String s, String t) {
@@ -23,6 +28,16 @@ public class Anagram {
         }
 
         return true;
+
+    }
+    public static boolean isAnagram2(String s, String t) {
+        var first = s.toCharArray();
+        var second = t.toCharArray();
+
+        Arrays.sort(first);
+        Arrays.sort(second);
+
+        return Arrays.equals(first, second);
 
     }
 
