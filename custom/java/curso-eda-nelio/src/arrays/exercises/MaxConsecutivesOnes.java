e463e27d7e97ceee4596521a471dcc1e946621fb
Eduardo J <duduxss3@gmail.com>
2025-04-02 10:19:58 -0300

    :sparkles: (main) Adiciona novo exercicio de arrays

---
+++ custom/java/curso-eda-nelio/src/arrays/exercises/MaxConsecutivesOnes.java
@@ -0,0 +1,27 @@
+package src.arrays.exercises;
+
+public class MaxConsecutivesOnes {
+
+    public static void main(String[] args) {
+        System.out.println(findMaxConsecutivesOnes(new int[]{1, 1, 0, 1, 1, 1}));
+        System.out.println(findMaxConsecutivesOnes(new int[]{1, 0, 1, 1, 0, 1}));
+    }
+
+    public static int findMaxConsecutivesOnes(int[] nums) {
+        var qtd = 0;
+        var maxValue = qtd;
+        for (var i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                qtd++;
+                if(qtd >= maxValue) {
+                    maxValue = qtd;
+                }
+            } else {
+                qtd = 0;
+            }
+        }
+
+        return maxValue;
+    }
+
+}
