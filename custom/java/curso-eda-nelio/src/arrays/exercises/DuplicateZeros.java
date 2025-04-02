394762c61c6fadcdb029fcef3fbec341c1a7d86e
Eduardo J <duduxss3@gmail.com>
2025-04-02 12:19:13 -0300

    :sparkles: (main) Adiciona nova task

---
+++ custom/java/curso-eda-nelio/src/arrays/exercises/DuplicateZeros.java
@@ -0,0 +1,30 @@
+package src.arrays.exercises;
+
+import java.util.Arrays;
+
+public class DuplicateZeros {
+
+    public static void main(String[] args) {
+        var arr = new int[] { 1,0,2,3,0,4,5,0 };
+        duplicateZeros(arr);
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+
+    public static void duplicateZeros(int[] arr) {
+        final var duplicatedZeroArray = new int[arr.length];
+        var j = 0;
+
+        for (var i = 0; i < arr.length && j < arr.length; i++) {
+            if(arr[i] != 0) {
+                duplicatedZeroArray[j] = arr[i];
+                ++j;
+            } else {
+                j+=2;
+            }
+        }
+
+        for(var i = 0 ; i < arr.length ; i++) {
+            arr[i] = duplicatedZeroArray[i];
+        }
+    }
+}
