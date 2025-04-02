5fae3929e8f0cb7695f014ffcf67721672384fae
Eduardo J <duduxss3@gmail.com>
2025-04-02 13:47:58 -0300

    :sparkles: (main) Adiciona nova solução pra task duplicate zeros

--- custom/java/curso-eda-nelio/src/arrays/exercises/DuplicateZeros.java
+++ custom/java/curso-eda-nelio/src/arrays/exercises/DuplicateZeros.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class DuplicateZeros {
 
     public static void main(String[] args) {
-        var arr = new int[] { 1,0,2,3,0,4,5,0 };
-        duplicateZeros(arr);
+        var arr = new int[]{1, 0, 2, 3, 0, 4, 5, 0};
+        duplicateZeros2(arr);
         Arrays.stream(arr).forEach(System.out::println);
     }
 
@@ -15,15 +15,30 @@ public class DuplicateZeros {
         var j = 0;
 
         for (var i = 0; i < arr.length && j < arr.length; i++) {
-            if(arr[i] != 0) {
+            if (arr[i] != 0) {
                 duplicatedZeroArray[j++] = arr[i];
             } else {
-                j+=2;
+                j += 2;
             }
         }
 
-        for(var i = 0 ; i < arr.length ; i++) {
+        for (var i = 0; i < arr.length; i++) {
             arr[i] = duplicatedZeroArray[i];
+        }
+    }
+
+    public static void duplicateZeros2(int[] arr) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] == 0 && i + 1 < arr.length) {
+                var nextElement = arr[i + 1];
+                arr[i + 1] = 0;
+                i++;
+                for (var j = i + 1; j < arr.length; j++) {
+                    var currentElement = arr[j];
+                    arr[j] = nextElement;
+                    nextElement = currentElement;
+                }
+            }
         }
     }
 }
