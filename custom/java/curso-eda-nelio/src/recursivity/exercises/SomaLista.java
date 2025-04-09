4ad0520da546ab2ab3de0a4067d4ab701833f2ce
Eduardo J <duduxss3@gmail.com>
2025-04-09 18:06:27 -0300

    :bug: (main) Adiciona mais exemplos ao SomaLista

--- custom/java/curso-eda-nelio/src/recursivity/exercises/SomaLista.java
+++ custom/java/curso-eda-nelio/src/recursivity/exercises/SomaLista.java
@@ -1,15 +1,15 @@
 package src.recursivity.exercises;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SomaLista {
 
     public static void main(String[] args) {
-        List<Double> lista = new ArrayList<>();
-        lista.addAll(List.of(4.0, 5.0, 3.0));
         System.out.println(sumList(List.of()));
-        System.out.println(sumList(lista));
+        System.out.println(sumList(List.of(4.0, 5.0, 3.0)));
+        System.out.println(sumList(List.of(4.0, 5.0)));
+        System.out.println(sumList(List.of(5.0, 5.0)));
+        System.out.println(sumList(List.of(5.0, 9.0)));
     }
 
     public static double sumList(List<Double> list) {
