f9ca048a0a34f0c4925abf8ded10adb41f377de2
Eduardo J <duduxss3@gmail.com>
2025-04-13 19:27:53 -0300

    :sparkles: (main) Adiciona notação assintótica na busca sequencial

--- custom/java/curso-eda-nelio/src/algorithms_complexity/exercises/BuscaSequencial.java
+++ custom/java/curso-eda-nelio/src/algorithms_complexity/exercises/BuscaSequencial.java
@@ -14,19 +14,23 @@ public class BuscaSequencial {
      *
      * Melhor caso: O elemento procurado é o primeiro da lista -> 1 passo
      * f(n) = 1 (função constante)
+     * Usando a anotação assintótica: Ω(1) => Omêga de 1.
      *
      * Pior caso: O elemento procurado é o último da lista, ou não existe -> n passos
      * f(n) = n (função linear)
+     * Usando a anotação assintótica: O(n) => Big O de N.
      *
      * Caso médio: O elemento procurado está em uma posição "qualquer" da lista -> n/2 passos
      * f(n) = n/2 (função linear)
+     * Usando a anotação assintótica: Θ(n) => Theta de N.
      *
      * > > > Análise da complexidade do espaço
      *
      * Nessa análise ignoramos a memória usada nos parâmetros passados (entrada), pois só queremos saber
      * o que o algoritmo utiliza de memória adicional, além dos parâmetros.
      * No caso abaixo então só temos a alocação de memória para a variável (i) que não se altera em função do tamanho
-     * da entrada, portanto a complexidade é f(n) = 1 (função constante)
+     * da entrada, portanto a complexidade em qualquer caso é f(n) = 1 (função constante)
+     * Usando a anotação assintótica: O(1), Θ(1), Ω(1) => Big O de 1, Theta de 1, Omêga de 1.
      */
     public static int buscaSequencial(final int[] vetor, final int elemento) {
         for (var i = 0; i < vetor.length; i++) {
